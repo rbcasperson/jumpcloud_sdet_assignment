5ec82c1dbaa17671ad5004c37d85789fece8e63f
Ryan Casperson <ryan.casperson@rackspace.com>
2019-06-12 15:30:31 -0400

    Add functionality to start a fresh API server
    This will be necessary to test accurate statistics

--- client.py
+++ client.py
@@ -1,11 +1,18 @@
+import subprocess
+
 import requests
 
 
 class HashServeClient(requests.Session):
-    def __init__(self, port):
+    def __init__(self, port, path_to_executable):
         super().__init__()
         self.port = port
+        self.path_to_executable = path_to_executable
         self.base_url = f"http://127.0.0.1:{port}"
+
+    def start(self):
+        """Start a fresh instance of the API server."""
+        subprocess.Popen([self.path_to_executable], env={"PORT": str(self.port)})
 
     def create_hash(self, password):
         return self.post(f"{self.base_url}/hash", json={"password": password})
--- tests/environment.py
+++ tests/environment.py
@@ -1,7 +1,17 @@
 from client import HashServeClient
 
 HASH_SERVE_PORT = 8088
+# I've hard-coded this in here and checked the binary into the repo
+# to make things work for me, and make it clear how things were working for me.
+# Ideally, this executable would be set up with a common name regardless of OS,
+# but that is beyond the scope of this assignment.
+PATH_TO_EXECUTABLE = "./bin/broken-hashserve_darwin"
 
 
 def before_all(context):
-    context.client = HashServeClient(HASH_SERVE_PORT)
+    context.client = HashServeClient(HASH_SERVE_PORT, PATH_TO_EXECUTABLE)
+    context.client.start()
+
+
+def after_all(context):
+    context.client.shut_down()
